8594f0f6ca4d2a31418fabde87fd36d046fd91f7
Huanjia Liang <huanjial@uci.edu>
2023-05-03 20:31:28 -0400

    Try to merge the user and its test to cooperate the main.py and other py files

--- src/schemas.py
+++ src/schemas.py
@@ -1,4 +1,6 @@
 # ItemCreate: the data required to create an item. 
 # Item: the data that is returned when the items are queried.
 # ItemBase: the fields that are common to ItemCreate and Item. This is to avoid duplication.
-
+from pydantic import BaseModel
+class InsertUserItem(BaseModel):
+    userID: str
