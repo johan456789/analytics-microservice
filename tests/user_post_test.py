301f38add967cce8fe2b64bbc5267907f52bd74b
Huanjia(Sam) Liang <huanjial@uci.edu>
2023-05-03 21:38:37 -0400

    Latest lianghuanjia (#20)
    Co-authored-by: Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>

--- tests/user_post_test.py
+++ tests/user_post_test.py
@@ -7,6 +7,8 @@ from src.user import app
 
 
 client = TestClient(app)
+
+
 def test_add_user_successfully():
     id = uuid.uuid4()
     payload = {"userID":str(id)}
@@ -17,35 +19,29 @@ def test_add_user_successfully():
     assert json_response['message'] == "Added user successfully"
     delete_user_from_database(str(id))
 
-#
-# def test_add_duplicate_user():
-#     try:
-#         id = uuid.uuid4()
-#         payload = {"userID":str(id)}
-#         response = client.post("/api/analysis/add-user/",json=payload)
-#         assert response.status_code == 200
-#         response = client.post("/api/analysis/add-user/",json=payload)
-#         # assert response.status_code == 400
-#         # json_response = json.loads(response.content.decode('utf-8'))
-#         # assert json_response['status code'] == 400
-#         # assert json_response['message'] == "User with the provided userID already exists"
-#     except HTTPException as e:
-#         print(e)
-#
-#
-# def test_missing_user_id_field():
-#     payload = {"userID": ""}
-#     response = client.post("/api/analysis/add-user/",json=payload)
-#     assert response.status_code == 400
-#     json_response = json.loads(response.content.decode('utf-8'))
-#     assert json_response['status code'] == 400
-#     assert json_response['message'] == "Missing field: userID"
-#
-# def test_add_user_userid_not_uuid():
-#     not_user_id = "not_uuid"
-#     payload = {"userID": not_user_id}
-#     response = client.post("/api/analysis/add-user/",json=payload)
-#     assert response.status_code == 400
-#     json_response = json.loads(response.content.decode('utf-8'))
-#     assert json_response['status code'] == 400
-#     assert json_response['message'] == "Provided userID is not UUID"
+
+def test_add_duplicate_user():
+    try:
+        id = uuid.uuid4()
+        payload = {"userID":str(id)}
+        response = client.post("/api/analysis/add-user/",json=payload)
+        assert response.status_code == 200
+        response = client.post("/api/analysis/add-user/",json=payload)
+        assert response.status_code == 413
+        delete_user_from_database(str(id))
+    except HTTPException as e:
+        print(e)
+        assert False
+
+
+def test_missing_user_id_field():
+    payload = {"userID": ""}
+    response = client.post("/api/analysis/add-user/",json=payload)
+    assert response.status_code == 411
+
+
+def test_add_user_userid_not_uuid():
+    not_user_id = "not_uuid"
+    payload = {"userID": not_user_id}
+    response = client.post("/api/analysis/add-user/",json=payload)
+    assert response.status_code == 412
