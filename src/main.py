5b276d9fab73a6bcb8f74247a5805c0242bd8343
Huanjia Liang <huanjial@uci.edu>
2023-05-03 21:58:07 -0400

    added main.py, models.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from datetime import datetime, timedelta
 from typing import List
-from models import *
-from database import session
+from .models import *
+from .database import session
 
 
 app = FastAPI()
